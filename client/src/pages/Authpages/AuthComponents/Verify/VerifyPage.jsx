9c0a4f37451167d51811ed3543b6f4aac847a424
kumarananda <coderanandakumar@gmail.com>
2022-08-15 03:55:47 +0600

    add a new page for verify user

---
+++ client/src/pages/Authpages/AuthComponents/Verify/VerifyPage.jsx
@@ -0,0 +1,34 @@
+
+import React from 'react'
+import './VerifyPage.scss'
+import imge_ga from "../../AuthImges/authlogo.png"
+
+
+const VerifyPage = () => {
+
+
+  return (
+    <>
+      <div className="verify-page">
+        <div className="page-cont">
+            <div className="imge-box">
+              <img src={imge_ga} alt="" />
+            </div>
+          <div className="verify-box">
+
+            <div className="items">
+              <h4>Please verify your mail</h4>
+              <p><a href="#">Send</a> a verify link</p>
+              <p><a href="#">Resend</a> link</p>
+            </div>
+
+          </div>
+        </div>
+        
+      </div>
+    
+    </>
+  )
+}
+
+export default VerifyPage
