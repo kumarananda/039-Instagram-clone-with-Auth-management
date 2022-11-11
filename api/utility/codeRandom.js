2f8090303ee434809f1ee55f1616036253246c4e
kumarananda <coderanandakumar@gmail.com>
2022-11-11 16:57:41 +0600

    get random code in javaScript

--- api/utility/codeRandom.js
+++ api/utility/codeRandom.js
@@ -1,6 +1,23 @@
+  
+  // update by me >> get random code in javaScript 
+  export const getrandCode = (length = 6 ) => {
+
+    // const rand = () =>  JSON.stringify(Math.floor(Math.random() * 10)) 
+    const rand = () => Math.floor(Math.random() * 10)
+    let code = []
+    for (let i = 0; i < length; i++) {
+      code.push(rand())
+    }
+  
+    let codeString = code.toString().replace(/,/g, '')
+  
+    return codeString;
+  }
+  
 
 
-export const getrandCode = (length) => {
+
+export const getrandCode_OLD = (length) => {
     const rand = () =>  JSON.stringify(Math.floor(Math.random() * 10)) 
     if(length == 4){
       const result = rand() + rand() + rand() + rand();
@@ -23,4 +40,6 @@ export const getrandCode = (length) => {
     }
 
   }
+
+
 
